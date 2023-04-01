22f4e36b58fcbb144754e935ab4d18dee51c8749
Elena <v-elenasa@microsoft.com>
2023-04-01 21:17:37 +0400

    Updated backend endpoint

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const IS_DEBUG = window.location.href === 'http://192.168.1.3:3000/'
 
-export const ENDPOINT = IS_DEBUG ? 'http://192.168.1.3:8000/' : 'https://parcheesi.herokuapp.com/'
+export const ENDPOINT = IS_DEBUG ? 'http://192.168.1.3:8000/' : 'https://16.171.45.77:8000/'
 
 export const VK_ACCESS_TOKEN = 'dfc81daadfc81daadfc81daa51dfba9a4cddfc8dfc81daa812a9735657f3387f235945d'
