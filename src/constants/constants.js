113384558cf6dd97301d998559f5c90f069edda8
Elena <v-elenasa@microsoft.com>
2023-04-15 01:27:01 +0400

    Fixed wrong host

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
-const LOCATION_HOST = 'ec2-16-171-45-77.eu-north-1.compute.amazonaws.com:8000'
+const LOCATION_HOST = 'ec2-16-171-45-77.eu-north-1.compute.amazonaws.com'
 
-export const IS_DEBUG = window.location.host !== LOCATION_HOST
+export const IS_DEBUG = window.location.host !== `${LOCATION_HOST}:3000`
 
-export const ENDPOINT = IS_DEBUG ? 'http://localhost:8000/' : `http://${LOCATION_HOST}/`
+export const ENDPOINT = IS_DEBUG ? 'http://localhost:8000/' : `http://${LOCATION_HOST}:8000/`
 
 export const VK_ACCESS_TOKEN = 'dfc81daadfc81daadfc81daa51dfba9a4cddfc8dfc81daa812a9735657f3387f235945d'
